f9d899982f32a765c0ebf99221a0092c96687264
Dave Espineda <despineda@thinklogic.net>
2026-06-11 22:54:33 +0800

    chore: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss({
+      optimize: {
+        minify: true
+      }
+    })],
+  },
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Varela Round",
+    cssVariable: "--font-varela-round",
+  }],
+});
